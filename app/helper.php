b76c0d297e59c828217b1db964ba06d438b7e4a0
Thuong L <ryan.imo80@gmail.com>
2019-06-13 10:40:45 +0700

    Fix Size Chart

--- app/helper.php
+++ app/helper.php
@@ -13,7 +13,7 @@ function clothing_config($type = '')
 				'front' =>array(460, 350),
 				'back' =>array(420, 200)
 			],
-			'size_chart' => 'https://images.bonanzastatic.com/afu/images/7e53/5101/1c12_6912388225/s-l1600.jpg'
+			'size_chart' => 'https://images-na.ssl-images-amazon.com/images/I/61th7k-im1L._UL1500_.jpg',//https://images.bonanzastatic.com/afu/images/7e53/5101/1c12_6912388225/s-l1600.jpg'
 		],
 		['title'=>'Women T-Shirt', 'name'=>'women-shirt', 'short_code'=>'WS', 'max_size'=>'3XL',
 			'color'=>['black','navy','red','royal'],
@@ -21,14 +21,14 @@ function clothing_config($type = '')
 				'front' =>array(475, 370),
 				'back' =>array(450, 350)
 			],
-			'size_chart' => 'https://cdn.shopify.com/s/files/1/2219/3745/files/chart_-_G500L_Gildan_Ladies_5.3_oz._T-Shirt.png?11730716537790178580'
+			'size_chart' => 'https://images-na.ssl-images-amazon.com/images/I/61M9Bnh9TEL._UL1368_.jpg',//https://cdn.shopify.com/s/files/1/2219/3745/files/chart_-_G500L_Gildan_Ladies_5.3_oz._T-Shirt.png?11730716537790178580'
 		],
 		['title'=>'Women Racerback Tank', 'name'=>'women-tank', 'short_code'=>'WT', 'max_size'=>'2XL',
 			'color'=>['black','navy','red','royal'],
 			'print_location' =>[
 				'front' => array(475, 500),
 			],
-			'size_chart' => 'https://cdn.shopify.com/s/files/1/2219/3745/files/chart_-_NL1533_Next_Level_Ladies_Ideal_Racerback_Tank.png?11730716537790178580'
+			'size_chart' => 'https://images-na.ssl-images-amazon.com/images/I/615FEHETCnL._UL1254_.jpg';//https://cdn.shopify.com/s/files/1/2219/3745/files/chart_-_NL1533_Next_Level_Ladies_Ideal_Racerback_Tank.png?11730716537790178580'
 		],
 		['title'=>'Tank Top', 'name'=>'tank-top', 'short_code'=>'TT', 'max_size'=>'2XL',
 			'color'=>['black','navy','red','royal'],
@@ -36,7 +36,7 @@ function clothing_config($type = '')
 				'front' => array(460, 470),
 				'back' => array(410, 200)
 			],
-			'size_chart' => 'https://cdn.shopify.com/s/files/1/2298/1873/files/TANK_3480_SIZE_CHART_1024x1024.jpg?v=1525747357'
+			'size_chart' => 'https://images-na.ssl-images-amazon.com/images/I/51jJ9JGlqwL._UL1023_.jpg',//https://cdn.shopify.com/s/files/1/2298/1873/files/TANK_3480_SIZE_CHART_1024x1024.jpg?v=1525747357'
 		],
 		['title'=>'Hoodie', 'name'=>'hoodie', 'short_code'=>'HD', 'max_size'=>'5XL',
 			'color'=>['black','navy','red','royal'],
@@ -44,7 +44,7 @@ function clothing_config($type = '')
 				'front' => array(460, 400),
 				'back' => array(440, 370)
 			],
-			'size_chart' => 'https://images-na.ssl-images-amazon.com/images/I/71DmC6g4syL._UL1500_.jpg'
+			'size_chart' => 'https://images-na.ssl-images-amazon.com/images/I/71DmC6g4syL._UL1500_.jpg'//https://images-na.ssl-images-amazon.com/images/I/61vkXzotMLL._UL1500_.jpg
 		],
 
 		// mug 11oz & 15oz
